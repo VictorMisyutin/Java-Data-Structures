196a88cf6e3824b653a9fb1512cb4bbb32591f6a
Victor Misyutin <misyutinvictor@gmail.com>
2023-09-20 19:04:48 -0500

    Lab 4 Init

---
+++ Lab 4/ArraysLab.java
@@ -0,0 +1,23 @@
+public class ArrayLab{
+    public static void main (String[] args){
+
+    }
+
+    public void linSearchString(String strArray[], String search)
+    {
+        int i = 0;
+        int flag = 0;
+        for(i = 0; i < 10; i++)
+        {
+            if (search.equals(strArray[i]))
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1)
+            System.out.println("item found at position " + (i + 1));
+        else
+            System.out.println("item not found");
+    }
+}
