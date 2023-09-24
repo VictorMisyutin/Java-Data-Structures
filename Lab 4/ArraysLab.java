ea027d8dbf15e9e8e3b8b3a318cbcfae591a69b9
Victor Misyutin <misyutinvictor@gmail.com>
2023-09-23 23:49:22 -0500

    more lab 4 progress

--- Lab 4/ArraysLab.java
+++ Lab 4/ArraysLab.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class ArraysLab{
     static Scanner scanner = new Scanner(System.in);
+    static String[] myClientsArray = new String[10];
+    static int[] myClientNumbers = new int[10];
     public static void main (String args[]){
-        // Create String Array
-        String[] myClientsArray = new String[10];
+        // Create Clients
         myClientsArray[0] = "Butler";
         myClientsArray[1] = "Samuels";
         myClientsArray[2] = "Bond";
@@ -13,9 +15,10 @@ public class ArraysLab{
         myClientsArray[5] = "Davis";
         myClientsArray[6] = "Zheng";
         myClientsArray[7] = "Joe";
+        myClientsArray[8] = "Dylan";
+        myClientsArray[9] = "Victor";
 
-        // Create Int Array
-        int[] myClientNumbers = new int[10];
+        // Create Client Numbers
         myClientNumbers[0] = 108;
         myClientNumbers[1] = 121;
         myClientNumbers[2] = 188;
@@ -24,22 +27,75 @@ public class ArraysLab{
         myClientNumbers[5] = 111;
         myClientNumbers[6] = 203;
         myClientNumbers[7] = 135;
+        myClientNumbers[8] = 189;
+        myClientNumbers[9] = 136;
 
-        // Search for name
+        // Search for client using linear search
         System.out.println("Enter a Client Name: ");
         String nameSearch = scanner.nextLine();
         int stringSearchVal = linSearchString(myClientsArray, nameSearch);
-
-        // Search for number
+        
+        // Search for number using linear search
         System.out.println("Enter a Client Number: ");
         int numSearch = scanner.nextInt();
         int intSearchVal = linSearchInt(myClientNumbers, numSearch);
         
+        // print results
         if ((intSearchVal != -1 && stringSearchVal != -1) && intSearchVal == stringSearchVal)
             System.out.println("results:                 the positions match");
         else 
             System.out.println("results:                 the positions do not match");
 
+        // Print both arrays
+        printParallelArrays(myClientsArray, myClientNumbers);
+
+        // Create new Arrays
+        String[] newClients1 = myClientsArray;
+        int[] newNumbers1 = myClientNumbers;
+
+        // sort both arrays using intrinsic sort
+        // Arrays.sort(newClients1);
+        // Arrays.sort(newNumbers1);
+
+        // search both arrays using binary search
+        stringSearchVal = binSearchString(newClients1, "Zheng");
+        intSearchVal = binSearchInt(newNumbers1, 203);
+
+        System.out.println("-------------" + stringSearchVal + " : " + intSearchVal + "----------");
+    
+    }
+    
+    // find first number in array and change name of responding client
+    public static void changeClient(int value, String newClient){
+        int index = linSearchInt(myClientNumbers, value);
+        myClientsArray[index] = newClient;
+    }
+
+    public static void removeClient(String client){
+        int index = linSearchString(myClientsArray, client);
+        myClientsArray[index] = null;
+        myClientNumbers[index] = 0;
+    }
+
+    public static void changeValue(String client, int newVal){
+        int index = linSearchString(myClientsArray, client);
+        myClientNumbers[index] = newVal;
+    }
+
+    public static void addValue(String client, int number){
+        for(int i = 0; i < myClientsArray.length;i++){
+            if(myClientsArray[i] == null){
+                myClientsArray[i] = client;
+                myClientNumbers[i] = number;
+            }
+        }
+    }
+
+    public static void printParallelArrays(String[] strArray,int[] intArray){
+        for(int i = 0; i<strArray.length;i++){
+            if (strArray[i] != null)
+                System.out.println(strArray[i] + ":" + intArray[i]);
+        }
     }
 
     public static int linSearchString(String strArray[], String search)
@@ -81,12 +137,98 @@ public class ArraysLab{
         return 0;
     }
     
-    public static void binSearchString(String strArray[], String search){
+    // precondition: strArray is sorted
+    public static int binSearchString(String strArray[], String search){  
+        int left = 0;
+        int right = strArray.length-1;
+        int middle,middleVal;
+        while(left<=right){
+            middle = left + (right-left)/2;
+            middleVal = search.compareTo(strArray[middle]);
+            
+            if(middleVal == 0) return middle;
+        
+            if(middleVal > 0) left = middle+1;
+            else right = middle-1;
+        }
+        return -1;
+    }
+ 
+    // precondition: intArray is sorted
+    public static int binSearchInt(int intArray[], int search){
+        int left = intArray[0];
+        int right = intArray.length-1;
+        int middle,middleVal;
+        while(left<=right){
+            middle = left + (right-left)/2;
+            middleVal = intArray[middle];
+            if(middleVal == search) return middle;
+        
+            if(middleVal < search) left = middle+1;
+            else right = middle-1;
+        }
+
+        return -1;
+    }
+
+    public static void stringBubbleSort(String[] strArray, int[] intArray){
+        boolean swapped = false;
+        String tempStr;
+        int tempInt;
+        for (int j = 0; j < strArray.length; j++) {
+            swapped = false; 
+            for (int i = j + 1; i < strArray.length; i++) {
+                // comparing adjacent strings
+                if (strArray[i].compareTo(strArray[j]) < 0) {
+                    tempStr = strArray[j];
+                    tempInt= intArray[j];
+                    strArray[j] = strArray[i];
+                    intArray[j] = intArray[i];
+                    strArray[i] = tempStr;
+                    intArray[i] = tempInt;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    public static void intBubbleSort(String[] strArray, int[] intArray){
+        boolean swapped;
+        int tempInt;
+        String tempStr;
+        for(int i = 0 ; i < intArray.length;i++){
+            swapped = false;
+            for(int j = 0; j < intArray.length-i-1;i++){
+                if(intArray[j] > intArray[j+1]){
+                    tempInt = intArray[j+1];
+                    tempStr = strArray[j+1];
+                    intArray[j+1] = intArray[i];
+                    strArray[j+1] = strArray[i];
+                    intArray[j] = tempInt;
+                    strArray[j] = tempStr;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }        
+    }  
+
+    public static void stringInsertionSort(String[] strArray){
 
     }
 
-    public static void binSearchInt(int intArray[], int search){
-
+    public static void intInsertionSort(int[] intArray){
+        int value;
+        for (int i = 1; i< intArray.length;i++){
+            value = intArray[i];
+            int j = i-1;
+            while(j>=0 && intArray[j] > value){
+                intArray[j+1] =intArray[j];
+                j--;                
+            }
+            intArray[j+1] = value;
+        }
     }
 
 }
