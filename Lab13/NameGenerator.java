d37a0c97c5d66da16772d94af171ac42436429a1
Victor Misyutin <misyutinvictor@gmail.com>
2023-11-27 12:18:00 -0600

    Completed Lab 13

--- Lab13/NameGenerator.java
+++ Lab13/NameGenerator.java
@@ -1,10 +1,13 @@
 package Lab13;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.io.File; 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;  
 
@@ -36,15 +39,47 @@ public class NameGenerator {
             e.printStackTrace();
         }
 
-        
+        // read names from file and display data
+        try {
+            File myObj = new File(inputFileName);
+            Scanner myReader = new Scanner(myObj);
+            int dupeCount = 0;
+            HashMap<String,Integer> dupeMap = new HashMap<>();
+            int i = 1;
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] namesArr = data.split(" "); 
+                for(String name : namesArr){ // find the duplicates on each line
+                    if(dupeMap.containsKey(name)){
+                        dupeMap.put(name, dupeMap.get(name)+1);
+                        dupeCount++;
+                    }
+                    else{
+                        dupeMap.put(name, 0);
+                    }
+                }
+                List<String> noDupsList = new ArrayList<>(new HashSet<>(Arrays.asList(data.split(" ")))); // remove duplicates using hashset
+                Collections.sort(noDupsList); // sort names list
+                System.out.println("Line " + i + ": ");
+                for (String name : noDupsList) { // print out sorted names with no duplicates
+                    System.out.print(name + " ");
+                }
+                System.out.println();
+                i++;
+            }
+            System.out.println("Number of duplicates found: " + dupeCount);
+            System.out.println("Duplicate count by name");
 
+            for(java.util.Map.Entry<String, Integer> entry : dupeMap.entrySet()){
+                System.out.println(entry.getKey() + ": " + entry.getValue() + " duplicates");
+            }
 
-        // // code for removing duplicates
-        // System.out.println();
-        // List<String> noDupsList = new ArrayList<>(new HashSet<>(names)); // remove duplicates using hashset
-        // Collections.sort(noDupsList); // sort names list
-        // for (String name : noDupsList) { // print out sorted names with no duplicates
-        //     System.out.println(name + " ");
-        // }
+            myReader.close();
+        } 
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
     }
 }
